01e0e54b31fa8a0f56b39b313c927c5346b10abd
Vladyslav <101827554+vladivsc@users.noreply.github.com>
2023-02-17 19:45:59 +0100

    upd

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,19 @@ import { NavLink } from 'react-router-dom';
 
 import styles from '../Navbar/navbar.module.scss';
 
-import items from "./items";
-
-const Navbar = ()=> {
-  const elements = items.map(({id, text, link})=> <li key={id} className={styles.item}>
-    <NavLink className={styles.link} to={link}>{text}</NavLink>
-  </li>)
-
-  return(
+const Navbar = () => {
+  return (
     <div className={styles.menu}>
       <ul className={styles.menuList}>
-    {elements}
-  </ul></div>
-  )
-}
+        <NavLink className={styles.link} to="/">
+          Home
+        </NavLink>
+        <NavLink className={styles.link} to="/movies">
+          Movies
+        </NavLink>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
